5cc58990252e60139db50449a4dd1466a6c4d532
“Ankush <ankush.sankhe@fractal.ai>
2020-04-29 16:21:35 +0530

    Updates

---
+++ precache-manifest.b16974df2af5ee298b38ed25d84a864b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b22af0c6f157c29a250fd965d8995a61",
+    "url": "/CovidLive/index.html"
+  },
+  {
+    "revision": "9f302a89d937e66889e0",
+    "url": "/CovidLive/static/css/main.13c8a689.chunk.css"
+  },
+  {
+    "revision": "bb89ecae4c3c3006f796",
+    "url": "/CovidLive/static/js/2.29e6e5ed.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/CovidLive/static/js/2.29e6e5ed.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9f302a89d937e66889e0",
+    "url": "/CovidLive/static/js/main.d9529dd1.chunk.js"
+  },
+  {
+    "revision": "b571903bde6e5f1437b8",
+    "url": "/CovidLive/static/js/runtime-main.6032882f.js"
+  },
+  {
+    "revision": "d7265326c3a1da5ec94c50746235f734",
+    "url": "/CovidLive/static/media/image.d7265326.png"
+  }
+]);
